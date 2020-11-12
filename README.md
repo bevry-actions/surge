18a3dc91bee244a3aa89b0710db1734421fdc39d
Benjamin Lupton <b@lupton.cc>
2020-11-12 14:22:52 +1100

    extracted from es-versions
    https://github.com/bevry/es-versions/tree/8c5e701543151d758f229489d75a5f55abbb8b1a/.github
    
    which extracted from awesome-travis
    
    https://github.com/bevry-archive/awesome-travis/blob/9fa2fa025c77b463d03154f39922af95cadc9c67/scripts/surge.bash

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# surge
-GitHub Action to publish your project to https://surge.sh
+# bevry-action/surge
+
+Once your tests succeed, use this action to deploy your commit, branch, and tag to [Surge](https://surge.sh).
+
+This allows you to git ignore your outputs (e.g. build files, documentation, etc) such that their changes don't make your repository dirty, nor pollute your git history, while allowing you to leverage CDN to referfence your source code and outputs, for every commit, branch, and tag.
+
+## Example
+
+For instance, for the [ambi](https://github.com/bevry/ambi) project.
+
+You can get the rendered documentation for:
+
+-   the [`master` branch](https://github.com/bevry/ambi/tree/master) via
+
+    > http://master.ambi.bevry.surge.sh/docs/globals.html
+
+-   the [`v8.22.0`](https://github.com/bevry/ambi/releases/tag/v6.6.0) tag via:
+
+    > http://v8.22.0.ambi.bevry.surge.sh/docs/globals.html
+
+-   the [`0250e1ed2cbc01773e0974165d1c9469c922b271` commit](https://github.com/bevry/ambi/commit/0250e1ed2cbc01773e0974165d1c9469c922b271) via:
+    > http://0250e1ed2cbc01773e0974165d1c9469c922b271.ambi.bevry.surge.sh/docs/globals.html
+
+## Install
+
+Make sure that the desired surge version you wish to use is installed locally:
+
+```bash
+npm install --save-dev surge
+```
+
+And add the following to your GitHub Action workflow after your tests have completed and you have built your compile targets/documentation.
+
+```yaml
+- uses: bevry-actions/surge@main
+  with:
+      surgeLogin: ${{secrets.SURGE_LOGIN}}
+      surgeToken: ${{secrets.SURGE_TOKEN}}
+```
+
+## License
+
+Public Domain via The Unlicense
---
+++ action.bash
@@ -0,0 +1,77 @@
+#!/bin/bash
+set -ueE -o pipefail
+
+# =====================================
+# AUTOMATIC GITHUB ENVIRONMENT VARIABLES
+# https://docs.github.com/en/free-pro-team@latest/actions/reference/environment-variables
+
+# GITHUB_REPOSITORY
+# GITHUB_REF
+# GITHUB_SHA
+
+# =====================================
+# MANUAL ENVIRONMENT VARIABLES
+
+# SURGE_LOGIN
+# SURGE_TOKEN
+# SURGE_PROJECT
+
+# =====================================
+# LOCAL ENVIRONMENT VARIABLES
+
+REPO_TAG=""
+REPO_BRANCH=""
+REPO_COMMIT=""
+if [[ "$GITHUB_REF" == "refs/tags/"* ]]; then
+	REPO_TAG="${GITHUB_REF#"refs/tags/"}"
+elif [[ "$GITHUB_REF" == "refs/heads/"* ]]; then
+	REPO_BRANCH="${GITHUB_REF#"refs/heads/"}"
+	REPO_COMMIT="$GITHUB_SHA"
+else
+	echo "unknown GITHUB_REF=$GITHUB_REF"
+	exit 1
+fi
+
+# org/name => name.org
+SURGE_SLUG="$(echo "$GITHUB_REPOSITORY" | sed 's/^\(.*\)\/\(.*\)/\2.\1/')"
+
+# defaults
+if test -z "${SURGE_PROJECT-}"; then
+	SURGE_PROJECT="."
+fi
+
+# =====================================
+# CHECKS
+
+if [[ "$REPO_BRANCH" = *"dependabot"* ]]; then
+	echo "skipping as running on a dependabot branch"
+	exit 0
+elif test -z "${SURGE_LOGIN-}" -o -z "${SURGE_TOKEN-}"; then
+	echo "you must provide a SURGE_LOGIN + SURGE_TOKEN combination"
+	exit 1
+fi
+
+# =====================================
+# RUN
+
+targets=()
+
+if test -n "$REPO_BRANCH"; then
+	targets+=("$REPO_BRANCH.$SURGE_SLUG.surge.sh")
+fi
+if test -n "$REPO_TAG"; then
+	targets+=("$REPO_TAG.$SURGE_SLUG.surge.sh")
+fi
+if test "$REPO_COMMIT"; then
+	targets+=("$REPO_COMMIT.$SURGE_SLUG.surge.sh")
+fi
+
+if test ${#targets[@]} -eq 0; then
+	echo 'failed to detect targets'
+	exit 1
+else
+	for target in ${targets[*]}; do
+		echo "deploying $SURGE_PROJECT to $target"
+		npx --no-install surge --project $SURGE_PROJECT --domain "$target"
+	done
+fi
